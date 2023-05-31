18ddf90b88c10385e1cf39ae1ea3830d45c0d2f8
Gabriel Garcia <garciasholding@gmail.com>
2023-05-31 17:18:44 -0300

    fix: migrations to match last changes

---
+++ PRIO/Migrations/20230531122016_WellNullableColumns.cs
@@ -0,0 +1,487 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PRIO.Migrations
+{
+    /// <inheritdoc />
+    public partial class WellNullableColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompletionId",
+                table: "Completions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodZone",
+                table: "Zones",
+                type: "VARCHAR(120)",
+                maxLength: 120,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(120)",
+                oldMaxLength: 120,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WellOperatorName",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "WaterDepth",
+                table: "Wells",
+                type: "float(10)",
+                precision: 10,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float(10)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TypeBaseCoordinate",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "TopOfPerforated",
+                table: "Wells",
+                type: "float(10)",
+                precision: 10,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float(10)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LongitudeDD",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Longitude4C",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LatitudeDD",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Latitude4C",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DatumHorizontal",
+                table: "Wells",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoordY",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoordX",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodWellAnp",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryReclassificationAnp",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryOperator",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryAnp",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "BaseOfPerforated",
+                table: "Wells",
+                type: "float(10)",
+                precision: 10,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float(10)",
+                oldPrecision: 10,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ArtificialLift",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "StatusOperator",
+                table: "Wells",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusOperator",
+                table: "Wells");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodZone",
+                table: "Zones",
+                type: "VARCHAR(120)",
+                maxLength: 120,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "WellOperatorName",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "WaterDepth",
+                table: "Wells",
+                type: "float(10)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float(10)",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TypeBaseCoordinate",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "TopOfPerforated",
+                table: "Wells",
+                type: "float(10)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float(10)",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LongitudeDD",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Longitude4C",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LatitudeDD",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Latitude4C",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DatumHorizontal",
+                table: "Wells",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoordY",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoordX",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodWellAnp",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryReclassificationAnp",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryOperator",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryAnp",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "BaseOfPerforated",
+                table: "Wells",
+                type: "float(10)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float(10)",
+                oldPrecision: 10,
+                oldScale: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ArtificialLift",
+                table: "Wells",
+                type: "VARCHAR(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompletionId",
+                table: "Completions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
