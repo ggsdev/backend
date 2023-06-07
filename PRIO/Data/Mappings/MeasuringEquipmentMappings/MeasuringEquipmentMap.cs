c478f77feb2a73affa85900e9733ff86a74b4fb6
Pablo Araujo <pabloaraujo_engmec@hotmail.com>
2023-06-07 15:37:25 -0300

    feat(equipment): fix models and map

--- PRIO/Data/Mappings/MeasuringEquipmentMappings/MeasuringEquipmentMap.cs
+++ PRIO/Data/Mappings/MeasuringEquipmentMappings/MeasuringEquipmentMap.cs
@@ -14,19 +14,60 @@ namespace PRIO.Data.Mappings.MeasuringEquipmentMapping
             builder.Property(x => x.Id)
                 .ValueGeneratedOnAdd();
 
-            builder.Property(x => x.Type)
-                .HasColumnType("varchar")
-                .HasMaxLength(120)
-                .IsRequired();
-
             builder.Property(x => x.TagEquipment)
                 .HasColumnType("varchar")
-                .HasMaxLength(120)
+                .HasMaxLength(60)
                 .IsRequired();
+
             builder.Property(x => x.TagMeasuringPoint)
                 .HasColumnType("varchar")
-                .HasMaxLength(120)
+                .HasMaxLength(60)
                 .IsRequired();
+            
+            builder.Property(x => x.SerieNumber)
+                .HasColumnType("varchar")
+                .HasMaxLength(60)
+                .IsRequired();
+
+            builder.Property(x => x.Type)
+                .HasColumnType("varchar")
+                .HasMaxLength(10)
+                .IsRequired();
+            
+            builder.Property(x => x.TypeEquipment)
+                .HasColumnType("varchar")
+                .HasMaxLength(60)
+                .IsRequired();
+            
+            builder.Property(x => x.Model)
+                .HasColumnType("varchar")
+                .HasMaxLength(60)
+                .IsRequired();
+            
+            builder.Property(x => x.HasSeal)
+                .IsRequired();
+            
+            builder.Property(x => x.MVS)
+                .IsRequired();
+            
+            builder.Property(x => x.CommunicationProtocol)
+                .HasColumnType("varchar")
+                .HasMaxLength(60)
+                .IsRequired();
+          
+            builder.Property(x => x.TypePoint)
+                .HasColumnType("varchar")
+                .HasMaxLength(60)
+                .IsRequired();
+                          
+            builder.Property(x => x.ChannelNumber)
+                .HasColumnType("varchar")
+                .HasMaxLength(10)
+                .IsRequired();
+
+            builder.Property(x => x.InOperation)
+                .IsRequired();
+
             builder.Property(x => x.Fluid)
                 .HasColumnType("varchar")
                 .HasMaxLength(120)
@@ -34,6 +75,14 @@ namespace PRIO.Data.Mappings.MeasuringEquipmentMapping
 
             builder.Property(x => x.Description)
                 .HasColumnType("text");
+
+            builder.Property(x => x.CreatedAt);
+
+            builder.Property(x => x.UpdatedAt);
+
+            builder.Property(x => x.DeletedAt);
+
+            builder.Property(x => x.IsActive);
 
             builder.HasOne(x => x.Installation)
                 .WithMany(d => d.MeasuringEquipments)
--- PRIO/Models/MeasuringEquipments/MeasuringEquipment.cs
+++ PRIO/Models/MeasuringEquipments/MeasuringEquipment.cs
@@ -7,10 +7,19 @@ namespace PRIO.Models.MeasuringEquipments
 {
     public class MeasuringEquipment : BaseModel
     {
-        public string Type { get; set; } = string.Empty;
-        public string TagEquipment { get; set; } = string.Empty;
-        public string TagMeasuringPoint { get; set; } = string.Empty;
-        public string Fluid { get; set; } = string.Empty;
+        public string? TagEquipment { get; set; } 
+        public string? TagMeasuringPoint { get; set; }
+        public string? SerieNumber { get; set; } 
+        public string? Type { get; set; }
+        public string? TypeEquipment { get; set; }
+        public string? Model { get; set; }
+        public bool? HasSeal { get; set; } 
+        public bool? MVS { get; set; } 
+        public string? CommunicationProtocol { get; set; } 
+        public string? TypePoint { get; set; } 
+        public string? ChannelNumber { get; set; } 
+        public string? InOperation { get; set; } 
+        public string? Fluid { get; set; }
         public Installation? Installation { get; set; }
         public List<Measurement>? Measurements { get; set; }
         public User? User { get; set; }
