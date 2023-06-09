7d65a08abdfd5a508fd5be88f553797e6d0cc0e5
Pablo Araujo <pabloaraujo_engmec@hotmail.com>
2023-06-09 10:03:51 -0300

    feat(equipment): create get, patch, delete and restore

--- PRIO/Controllers/EquipmentController.cs
+++ PRIO/Controllers/EquipmentController.cs
@@ -68,35 +68,151 @@ namespace PRIO.Controllers
             return Created($"equipments/{equipment.Id}", equipmentDTO);
         }
 
-        //[HttpGet("equipment")]
-        //public async Task<IActionResult> Get()
-        //{
+        [HttpGet("equipments")]
+        public async Task<IActionResult> Get()
+        {
+            var userId = (Guid)HttpContext.Items["Id"]!;
+            var user = await _context.Users.FirstOrDefaultAsync((x) => x.Id == userId);
+            if (user is null)
+                return NotFound(new ErrorResponseDTO
+                {
+                    Message = $"User is not found"
+                });
 
-        //}
+            var equipments = await _context.MeasuringEquipments.ToListAsync();
+            var equipmentDTO = _mapper.Map<List<MeasuringEquipment>, List<MeasuringEquipmentDTO>>(equipments);
+            return Ok(equipmentDTO);
 
-        //[HttpGet("equipment/{id}")]
-        //public async Task<IActionResult> GetById([FromRoute] Guid id)
-        //{
+        }
 
-        //}
+        [HttpGet("equipments/{id}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            var userId = (Guid)HttpContext.Items["Id"]!;
+            var user = await _context.Users.FirstOrDefaultAsync((x) => x.Id == userId);
+            if (user is null)
+                return NotFound(new ErrorResponseDTO
+                {
+                    Message = $"User is not found"
+                });
+            var equipment = _context.MeasuringEquipments.FirstOrDefault(x => x.Id == id);
+            if (equipment is null)
+                return NotFound(new ErrorResponseDTO
+                {
+                    Message = "Equipment not found"
+                });
+            var equipmentDTO = _mapper.Map<MeasuringEquipment, MeasuringEquipmentDTO>(equipment);
+            return Ok(equipmentDTO);
+        }
 
-        //[HttpPatch("equipment/{id}")]
-        //public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateClusterViewModel body)
-        //{
+        [HttpPatch("equipments/{id}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateEquipmentViewModel body)
+        {
+            var equipment = _context.MeasuringEquipments.Include(x => x.Installation).FirstOrDefault(x => x.Id == id);
+            if (equipment is null)
+                return NotFound(new ErrorResponseDTO
+                {
+                    Message = "Equipment not found"
+                });
 
-        //}
+            var userId = (Guid)HttpContext.Items["Id"]!;
+            var user = await _context.Users.FirstOrDefaultAsync((x) => x.Id == userId);
+            if (user is null)
+                return NotFound(new ErrorResponseDTO
+                {
+                    Message = $"User is not found"
+                });
 
-        //[HttpPatch("equipment/{id}/restore")]
-        //public async Task<IActionResult> Restore([FromRoute] Guid id)
-        //{
+            var installationInDatabase = await _context.Installations.FirstOrDefaultAsync(x => x.Id == body.InstallationId);
 
-        //}
+            if (body.InstallationId is not null && installationInDatabase is null)
+            {
+                return NotFound(new ErrorResponseDTO
+                {
+                    Message = "Installation not found"
+                });
+            }
 
-        //[HttpDelete("equipment/{id}")]
-        //public async Task<IActionResult> Delete([FromRoute] Guid id)
-        //{
+            equipment.TagEquipment = body.TagEquipment is not null ? body.TagEquipment : equipment.TagEquipment;
+            equipment.TagMeasuringPoint = body.TagMeasuringPoint is not null ? body.TagMeasuringPoint : equipment.TagMeasuringPoint;
+            equipment.SerieNumber = body.SerieNumber is not null ? body.SerieNumber : equipment.SerieNumber;
+            equipment.Type = body.Type is not null ? body.Type : equipment.Type;
+            equipment.TypeEquipment = body.TagEquipment is not null ? body.TagEquipment : equipment.TagEquipment;
+            equipment.Model = body.Model is not null ? body.Model : equipment.Model;
+            equipment.HasSeal = body.HasSeal is not null ? body.HasSeal : equipment.HasSeal;
+            equipment.MVS = body.MVS is not null ? body.MVS : equipment.MVS;
+            equipment.CommunicationProtocol = body.CommunicationProtocol is not null ? body.CommunicationProtocol : equipment.CommunicationProtocol;
+            equipment.TypePoint = body.TypePoint is not null ? body.TypePoint : equipment.TypePoint;
+            equipment.ChannelNumber = body.ChannelNumber is not null ? body.ChannelNumber : equipment.ChannelNumber;
+            equipment.InOperation = body.InOperation is not null ? body.InOperation : equipment.InOperation;
+            equipment.Fluid = body.Fluid is not null ? body.Fluid : equipment.Fluid;
+            equipment.Installation = installationInDatabase is not null ? installationInDatabase : equipment.Installation;
+            equipment.Description = body.Description is not null ? body.Description : equipment.Description;
 
-        //}
+            _context.MeasuringEquipments.Update(equipment);
+            await _context.SaveChangesAsync();
+
+            var equipmentDTO = _mapper.Map<MeasuringEquipment, MeasuringEquipmentDTO>(equipment);
+            return Ok(equipmentDTO);
+        }
+
+        [HttpDelete("equipments/{id}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var userId = (Guid)HttpContext.Items["Id"]!;
+            var user = await _context.Users.FirstOrDefaultAsync((x) => x.Id == userId);
+            if (user is null)
+                return NotFound(new ErrorResponseDTO
+                {
+                    Message = $"User not found"
+                });
+
+            var equipment = _context.MeasuringEquipments.FirstOrDefault(x => x.Id == id);
+            if (equipment is null || !equipment.IsActive)
+                return NotFound(new ErrorResponseDTO
+                {
+                    Message = "Equipment not found or inactive already"
+                });
+
+            equipment.IsActive = false;
+            equipment.DeletedAt = DateTime.UtcNow;
+
+            _context.MeasuringEquipments.Update(equipment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPatch("equipments/{id}/restore")]
+        public async Task<IActionResult> Restore([FromRoute] Guid id)
+        {
+            var userId = (Guid)HttpContext.Items["Id"]!;
+            var user = await _context.Users.FirstOrDefaultAsync((x) => x.Id == userId);
+            if (user is null)
+                return NotFound(new ErrorResponseDTO
+                {
+                    Message = $"User not found"
+                });
+
+            var equipment = _context.MeasuringEquipments.FirstOrDefault(x => x.Id == id);
+            if (equipment is null || equipment.IsActive)
+                return NotFound(new ErrorResponseDTO
+                {
+                    Message = "Equipment not found or active already"
+                });
+
+            equipment.IsActive = true;
+            equipment.DeletedAt = null;
+
+            _context.MeasuringEquipments.Update(equipment);
+            await _context.SaveChangesAsync();
+
+            var equipmentDTO = _mapper.Map<MeasuringEquipment, MeasuringEquipmentDTO>(equipment);
+
+            return Ok(equipmentDTO);
+
+        }
+
         //[HttpGet("equipment/{id}/history")]
         //public async Task<IActionResult> GetHistory([FromRoute] Guid id)
         //{
---
+++ PRIO/ViewModels/MeasuringEquipment/UpdateEquipmentViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace PRIO.ViewModels.MeasuringEquipment
+{
+    public class UpdateEquipmentViewModel
+    {
+        public string? TagEquipment { get; set; }
+        public string? TagMeasuringPoint { get; set; }
+        public string? SerieNumber { get; set; }
+        public string? Type { get; set; }
+        public string? TypeEquipment { get; set; }
+        public string? Model { get; set; }
+        public bool? HasSeal { get; set; }
+        public bool? MVS { get; set; }
+        public string? CommunicationProtocol { get; set; }
+        public string? TypePoint { get; set; }
+        public string? ChannelNumber { get; set; }
+        public bool? InOperation { get; set; }
+        public string? Fluid { get; set; }
+        public Guid? InstallationId { get; set; }
+        public string? Description { get; set; }
+    }
+}
