1e9e6d2f4da934d18b47ac56feb236ef25d0c9cd
Garcia <garciasholding@gmail.com>
2023-07-03 15:18:30 -0300

    fix: xls import history

--- PRIO/src/Shared/SystemHistories/Dtos/ImportHistoryDTO.cs
+++ PRIO/src/Shared/SystemHistories/Dtos/ImportHistoryDTO.cs
@@ -2,7 +2,6 @@
 {
     public class ImportHistoryDTO
     {
-        public Guid? Id { get; set; }
         public object? FileName { get; set; }
         public Guid? CreatedBy { get; set; }
         public DateTime? CreatedAt { get; set; }
