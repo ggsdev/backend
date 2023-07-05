5b0be4b6c61267944665c9abf85d48416fdb1074
Garcia <garciasholding@gmail.com>
2023-07-05 12:44:33 -0300

    feat: health check controller

---
+++ PRIO/src/Shared/Infra/Http/Controllers/HealthCheckController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PRIO.src.Shared.Infra.EF;
+
+namespace PRIO.src.Shared.Infra.Http.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly DataContext _dbContext;
+        public HealthCheckController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetHealth()
+        {
+            try
+            {
+                await _dbContext.Database.CanConnectAsync();
+                return Ok(new { Message = "API is healthy" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
