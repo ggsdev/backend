8b0c82c856ed149d43747d523d28faaa97c6e4c3
unknown <pabloaraujo_engmec@hotmail.com>
2023-07-11 14:46:21 -0300

    fix(group) include description

--- PRIO/src/Modules/ControlAccess/Groups/Dtos/GroupWithGroupPermissionDTO.cs
+++ PRIO/src/Modules/ControlAccess/Groups/Dtos/GroupWithGroupPermissionDTO.cs
@@ -4,6 +4,7 @@
     {
         public Guid? Id { get; set; }
         public string? Name { get; set; }
+        public string? Description { get; set; }
         public List<GroupPermissionParentDTO>? GroupPermissions { get; set; }
     }
 }
