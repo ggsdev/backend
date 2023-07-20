730945c57b4fbc58491892d3e44c077267b3d60e
Garcia <garciasholding@gmail.com>
2023-07-20 14:49:44 -0300

    feat: format errors in 001 file

--- PRIO/src/Modules/FileImport/XML/Infra/Utils/XmlUtils.cs
+++ PRIO/src/Modules/FileImport/XML/Infra/Utils/XmlUtils.cs
@@ -15,7 +15,7 @@ namespace PRIO.src.Modules.FileImport.XML.Infra.Utils
         internal static readonly string FileAcronym002 = "PMGL";
         internal static readonly string FileAcronym003 = "PMGD";
 
-        public static decimal? DecimalParser(string? valueToBeParsed, List<string> errors, string? nameElement = "")
+        public static decimal? DecimalParser(string? valueToBeParsed, List<string> errors, string? nameElement)
         {
             if (string.IsNullOrEmpty(valueToBeParsed))
                 return null;
@@ -26,6 +26,19 @@ namespace PRIO.src.Modules.FileImport.XML.Infra.Utils
                 errors.Add($"Modelo dados inválidos, valor: {valueToBeParsed}, tag: {nameElement} formato númerico aceitável: 000,000");
 
             return 0;
+        }
+
+        public static DateTime? DateTimeParser(string? valueToBeParsed, List<string> errors, string? nameElement)
+        {
+            if (string.IsNullOrEmpty(valueToBeParsed))
+                return null;
+
+            if (DateTime.TryParseExact(valueToBeParsed, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out var result))
+                return result;
+            else
+                errors.Add($"Modelo dados inválidos, valor: {valueToBeParsed}, tag: {nameElement}, formato de data aceitável: dd/MM/yyyy HH:mm:ss");
+
+            return null;
         }
     }
 }
