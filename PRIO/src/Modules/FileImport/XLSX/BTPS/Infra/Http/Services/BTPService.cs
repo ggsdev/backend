5882a61960b0f22a139b31a783cc7a5257a87728
unknown <pabloaraujo_engmec@hotmail.com>
2023-08-02 10:34:18 -0300

    feat(btps): finish render route

--- PRIO/src/Modules/FileImport/XLSX/BTPS/Infra/Http/Services/BTPService.cs
+++ PRIO/src/Modules/FileImport/XLSX/BTPS/Infra/Http/Services/BTPService.cs
@@ -86,6 +86,90 @@ namespace PRIO.src.Modules.FileImport.XLSX.BTPS.Infra.Http.Services
 
             var content = new BTPBase64
             {
+                Filename = body.FileName,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+                Type = body.Type,
+                FileContent = body.ContentBase64,
+                IsActive = true,
+                User = user
+            };
+            var data = new BTPData
+            {
+                Filename = body.FileName,
+                Type = body.Type,
+                PotencialGas = worksheet.Cells[BTP.CellPotencialGas].Value.ToString(),
+                PotencialOil = worksheet.Cells[BTP.CellPotencialOil].Value.ToString(),
+                PotencialWater = worksheet.Cells[BTP.CellPotencialWater].Value.ToString(),
+                Duration = valorTempo,
+                InitialDate = worksheet.Cells[BTP.CellInitialDate].Value.ToString(),
+                FinalDate = worksheet.Cells[BTP.CellFinalDate].Value.ToString(),
+                BTPNumber = worksheet.Cells[BTP.CellBTPNumber].Value.ToString(),
+                MPointGas = worksheet.Cells[BTP.CellMPointGas].Value.ToString(),
+                MPointOil = worksheet.Cells[BTP.CellMPointOil].Value.ToString(),
+                MPointWater = worksheet.Cells[BTP.CellMPointWater].Value.ToString(),
+                BSW = worksheet.Cells[BTP.CellBSW].Value.ToString(),
+                RGO = worksheet.Cells[BTP.CellRGO].Value.ToString(),
+                PotencialLiquid = worksheet.Cells[BTP.CellPotencialLiquid].Value.ToString(),
+                WellAlignmentData = worksheet.Cells[BTP.CellWellAlignmentData].Value.ToString(),
+                WellAlignmentHour = align,
+                WellName = worksheet.Cells[BTP.CellWellName].Value.ToString(),
+                BTPSheet = BTP.BTPSheet,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+                BTPBase64 = content,
+                Well = well
+            };
+
+            var BTPdataDTO = _mapper.Map<BTPData, BTPDataDTO>(data);
+            var createDataDTO = new CreateDataBTODTO
+            {
+                Message = message,
+                Data = BTPdataDTO
+            };
+
+            return createDataDTO;
+        }
+        public async Task<CreateDataBTODTO> PostImportFiles(RequestWellTestXls body, User user)
+        {
+            var BTP = await _BTPRepository.GetByIdAsync(body.BTPId) ?? throw new NotFoundException("BTP não encontrado.");
+
+            if (body.FileName.EndsWith(".xlsx") is false)
+                throw new BadRequestException("O arquivo deve ter a extensão .xlsx", status: "Error");
+
+            if (body.Type != BTP.Type)
+                throw new ConflictException($"O modelo do arquivo não corresponde ao tipo {body.Type}");
+
+            var well = await _wellRepository.GetByIdAsync(body.WellId) ?? throw new NotFoundException("Poço não encontrado.");
+
+            var contentBase64 = body.ContentBase64?.Replace("data:@file/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,", "");
+            using var stream = new MemoryStream(Convert.FromBase64String(contentBase64!));
+            using ExcelPackage package = new(stream);
+            var workbook = package.Workbook;
+            var worksheet = BTP.BTPSheet is not null ? workbook.Worksheets[BTP.BTPSheet] : workbook.Worksheets[0];
+
+            //Trated aligmentHour
+            string valorDaCelula = worksheet.Cells[BTP.CellWellAlignmentHour].Value.ToString();
+            bool checkAlignHour = decimal.TryParse(valorDaCelula, out var valor);
+            string? align = "";
+            if (checkAlignHour is true)
+            {
+                decimal x = valor * 24 * 60;
+                int horas = (int)x / 60;
+                int minutos = (int)x % 60;
+                TimeSpan horaMinuto = new TimeSpan(horas, minutos, 0);
+                align = horaMinuto.ToString();
+            }
+
+            //Trated Duration
+            string[] duration = worksheet.Cells[BTP.CellDuration].Value.ToString().Split(' ');
+            string valorTempo = duration[1];
+
+            //Verify Well
+            string? message = well.Name == worksheet.Cells[BTP.CellWellName].Value.ToString() ? "Sucess: Nome do poço encontrado corresponde ao xls" : "Warning: Nome do poço encontrado não corresponde ao xls";
+
+            var content = new BTPBase64
+            {
                 Id = Guid.NewGuid(),
                 Filename = body.FileName,
                 CreatedAt = DateTime.Now,
