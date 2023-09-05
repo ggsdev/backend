1f5e1cab4d6f624dd6b0835dd82c1babd750d8d0
unknown <pabloaraujo_engmec@hotmail.com>
2023-09-05 14:51:13 -0300

    fix(HangFire): refactor

--- PRIOScheduler/UpdateDowntime.cs
+++ PRIOScheduler/UpdateDowntime.cs
@@ -21,60 +21,18 @@ namespace PRIOScheduler
                 using var dbContext = new DataContext(dbContextOptions);
 
                 var dateYesterday = DateTime.UtcNow.AddHours(-3).Date.AddDays(-1);
+                var wellTests = await dbContext.WellEvents
+                    .Where(
+                    x => x.StartDate < dateYesterday && x.EndDate != null && x.EndDate.Value.Date == dateYesterday ||
+                    x.StartDate < dateYesterday && x.EndDate != null && x.EndDate.Value.Date > dateYesterday ||
+                    x.StartDate.Date == dateYesterday.Date && x.EndDate != null && x.EndDate.Value.Date == dateYesterday.Date ||
+                    x.StartDate.Date == dateYesterday.Date && x.EndDate != null && x.EndDate.Value.Date > dateYesterday.Date ||
+                    x.StartDate < dateYesterday && x.EndDate == null ||
+                    x.StartDate.Date == dateYesterday && x.EndDate == null
+                )
+                .ToListAsync();
 
-                var production = await dbContext.Productions
-                    .Include(p => p.WellProductions)
-                    .FirstOrDefaultAsync(x => x.MeasuredAt.Date == dateYesterday);
-
-                if (production is not null)
-                {
-                    var wellEvents = await dbContext.WellEvents
-                        .Include(we => we.Well)
-                        .Where(x => (production != null && x.StartDate <= production.MeasuredAt.Date && x.EndDate == null)
-                        || (production != null && x.StartDate.Date <= production.MeasuredAt.Date && x.EndDate != null && production.MeasuredAt.Date >= x.EndDate.Value.Date))
-                        .Where(x => x.EventStatus == "F")
-                        .ToListAsync();
-
-                    foreach (var wellEvent in wellEvents)
-                    {
-                        var totalDowtimeDaily = 0d;
-
-                        if (wellEvent.StartDate < production.MeasuredAt && wellEvent.EndDate is not null && wellEvent.EndDate.Value.Date == production.MeasuredAt)
-                        {
-                            totalDowtimeDaily += ((wellEvent.EndDate.Value - production.MeasuredAt).TotalMinutes) / 60;
-                        }
-                        else if (wellEvent.StartDate < production.MeasuredAt && wellEvent.EndDate is not null && wellEvent.EndDate.Value.Date > production.MeasuredAt)
-                        {
-                            totalDowtimeDaily += 24;
-                        }
-                        else if (wellEvent.StartDate.Date == production.MeasuredAt.Date && wellEvent.EndDate is not null && wellEvent.EndDate.Value.Date == production.MeasuredAt.Date)
-                        {
-                            totalDowtimeDaily += ((wellEvent.EndDate.Value - wellEvent.StartDate).TotalMinutes) / 60;
-                        }
-                        else if (wellEvent.StartDate.Date == production.MeasuredAt.Date && wellEvent.EndDate is not null && wellEvent.EndDate.Value.Date > production.MeasuredAt.Date)
-                        {
-                            totalDowtimeDaily += ((production.MeasuredAt.AddDays(1) - wellEvent.StartDate).TotalMinutes) / 60;
-                        }
-                        else if (wellEvent.StartDate < production.MeasuredAt && wellEvent.EndDate is null)
-                        {
-                            totalDowtimeDaily += 24;
-                        }
-                        else if (wellEvent.StartDate.Date == production.MeasuredAt && wellEvent.EndDate is null)
-                        {
-                            totalDowtimeDaily += ((production.MeasuredAt.AddDays(1) - wellEvent.StartDate).TotalMinutes) / 60;
-                        }
-
-
-                    }
-
-                }
-
-                foreach (var wellProduction in production.WellProductions)
-                {
-
-
-
-                }
+                Console.WriteLine(wellTests.Count);
 
                 Console.WriteLine($"Job executado com sucesso.");
 
