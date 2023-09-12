447e774b1281d48d8a1a3caa668782b07357f21a
unknown <pabloaraujo_engmec@hotmail.com>
2023-09-12 10:01:16 -0300

    fix

--- PRIOScheduler/UpdateDowntime.cs
+++ PRIOScheduler/UpdateDowntime.cs
@@ -59,7 +59,6 @@ namespace PRIOScheduler
                             }
                             var FirstReasonFormattedTime = $"{FirstReasonFormattedHours}:{firstFormattedMinutes}:{firstFormattedSecond}";
                             reason.Interval = FirstReasonFormattedTime;
-                            reason.IsJobGenerated = true;
 
                             DateTime refStartDate = reason.StartDate.Date.AddDays(1);
                             DateTime refStartEnd = refStartDate.AddDays(1).AddMilliseconds(-10);
