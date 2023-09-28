3047fa7ae7a2aa55773deeef6b25ae80e503bf0b
unknown <pabloaraujo_engmec@hotmail.com>
2023-09-28 16:03:15 -0300

    feat(PI): create populateMigrations

---
+++ PRIO/src/Modules/PI/Infra/EF/Models/Database.cs
@@ -0,0 +1,16 @@
+﻿namespace PRIO.src.Modules.PI.Infra.EF.Models
+{
+    public class Database
+    {
+        public Guid Id { get; set; }
+        public string WebId { get; set; }
+        public string PIId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SelfRoute { get; set; }
+        public string ElementsRoute { get; set; }
+
+        public List<Instance> Instances { get; set; }
+
+    }
+}
