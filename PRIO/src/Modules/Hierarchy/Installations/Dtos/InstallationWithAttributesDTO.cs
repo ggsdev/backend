ab3bb0f1ce7a57a8e8fab79d14747bc37898956c
Garcia <garciasholding@gmail.com>
2023-10-02 19:53:57 -0300

    merge: resolving conflicts

--- PRIO/src/Modules/Hierarchy/Installations/Dtos/InstallationWithAttributesDTO.cs
+++ PRIO/src/Modules/Hierarchy/Installations/Dtos/InstallationWithAttributesDTO.cs
@@ -14,6 +14,6 @@ namespace PRIO.src.Modules.Hierarchy.Installations.Dtos
         public string? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public bool? IsActive { get; set; }
-        public List<AttributeDTO> Attributes { get; set; }
+        public List<AttributeReturnDTO> Attributes { get; set; }
     }
 }
