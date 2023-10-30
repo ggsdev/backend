b437e70f54a82bf82d3cc66eb979aad4595cf550
Garcia <garciasholding@gmail.com>
2023-10-30 10:25:32 -0300

    fix: code generation and 003 file multiply 1000

--- PRIO/src/Shared/Auxiliaries/Infra/Http/Services/AuxiliaryService.cs
+++ PRIO/src/Shared/Auxiliaries/Infra/Http/Services/AuxiliaryService.cs
@@ -16,30 +16,33 @@ namespace PRIO.src.Shared.Auxiliaries.Infra.Http.Services
 
         public async Task<List<Auxiliary>> Get(string table, string route)
         {
+            var auxliaryList = await _context.Auxiliaries
+                .AsNoTracking()
+                .ToListAsync();
 
-            var validTables = new List<string>
-            {
-                "clusters", "installations", "fields", "zones", "reservoirs", "wells", "completions", "measuringequipments", "elemento primário", "elemento secundário", "teste", "wellevents", "equipado aguardando início de operação", "produzindo", "injetando", "produzindo e injetando","retirando gás natural estocado", "injetando para estocagem", "operando para captação de água", "fechado",  "abandonado permanentemente", "abandonado temporariamente com monitoramento","abandonado temporariamente sem monitoramento","arrasado", "em observação","em perfuração", "em avaliação", "em completação", "em intervenção", "cedido para captação de água","operando para descarte", "pressão", "vazão", "categories"
-            };
+            var tableParameter = table.ToUpper().Trim();
+            var routeParameter = route.ToUpper().Trim();
 
+            var validTables = auxliaryList
+                .Select(x => x.Table)
+                .Distinct()
+                .ToList();
 
-            if (validTables.Contains(table.ToLower()) is false)
+            if (!validTables.Contains(tableParameter.ToUpper()))
                 throw new BadRequestException($"Invalid table options are: {string.Join(", ", validTables)}");
 
+            var validRoutes = auxliaryList
+                .Select(x => x.Route)
+                .Distinct()
+                .ToList();
 
-            var validRoutes = new List<string>
-            {
-                "/cadastrosbasicos", "/importardadostestepoco", "/eventospoco", "/parametrospi"
-            };
-
-
-            if (validRoutes.Contains(route.ToLower()) is false)
+            if (!validRoutes.Contains(routeParameter.ToUpper()))
                 throw new BadRequestException($"Invalid route options are: {string.Join(" ", validRoutes)}");
 
-            var selectOptions = await _context.Auxiliaries
-                .Where(x => x.Table == table && x.Route == route)
+            var selectOptions = auxliaryList
+                .Where(x => x.Table != null && x.Table.ToUpper() == tableParameter && x.Route != null && x.Route.ToUpper() == routeParameter)
                 .OrderBy(x => x.Select)
-                .ToListAsync();
+                .ToList();
 
             return selectOptions;
         }
