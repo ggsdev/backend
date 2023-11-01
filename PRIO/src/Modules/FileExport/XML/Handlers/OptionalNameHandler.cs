397c92810a4082535ae65571fd2bd47bfefa3d4e
unknown <pabloaraujo_engmec@hotmail.com>
2023-11-01 11:02:55 -0300

    fix

---
+++ PRIO/src/Modules/FileExport/XML/Handlers/OptionalNameHandler.cs
@@ -0,0 +1,16 @@
+﻿using PRIO.src.Modules.FileExport.XML.ViewModels;
+
+namespace PRIO.src.Modules.FileExport.XML.Handlers
+{
+    public class OptionalNameHandler : FileNameHandler
+    {
+        public override string Handle(ExportXMLViewModel body, object model)
+        {
+            if (body.OptionalName != null)
+            {
+                return body.OptionalName;
+            }
+            return base.Handle(body, model);
+        }
+    }
+}
